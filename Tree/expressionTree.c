e10376654cba6516093e9f3d30d85403e13e4728
priya mohata <mohatapriya@gmail.com>
2020-09-28 11:15:16 +0530

    updated expressionTree.c

--- Tree/expressionTree.c
+++ Tree/expressionTree.c
@@ -9,12 +9,24 @@ struct Node
 struct Node *create(char *postfix)
 {
     int i,k=0;
+    struct Node *st[20];//Stack creation 
     char sym; //To read each symbol in the expression
     struct Node *temp=(struct Node *)malloc(sizeof(struct Node));//Creation of node for each symbol.
-    for(i=0;sym=postfix[i]!='\0';i++)
+    for(i=0;sym=postfix[i]!='\0';i++) //To read the symbol till we reach the end of the expression
     {
-        temp->rlink=NULL;
+        temp->rlink=NULL; //Creation of node for each symbol.
         temp->llink=NULL;
+        temp->data=sym;
     }
-
+    if(isalnum(sym))
+    {
+        st[k++]=temp; //To check if the symbol if alphabet or a digit we push the element
+    }
+    else
+    {
+        temp->rlink=st[--k]; //Else we pop it and assign the current symbol's rlink and link to the elements popped
+        temp->llink=st[--k];
+        st[k++]=temp;
+    }
+    return st[k--]; //To return the top of the stack
 }
